e31923a02d1f0d873a5c83eef41a95d2da14f03e
Tmajki <tm51226@student.uni-lj.si>
2023-02-21 12:45:12 +0100

    Dodaj izracun pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,18 @@
+import java.util. *;
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
+		Scanner sc = new Scanner(System.in);
+		int v = sc.nextInt();  //nadmorska visina
+		
+		
+		
     }
+	public static int izracunaj(int v) {
+		int C = 6.674 * Math.pow(10, -11); //gravitacijska konstanta
+		int M = 5.972 * Math.pow(10, 24); // masa Zemlje
+		int R = 6.371 * Math.pow(10, 6); //polmer Zemlje
+	    int a = (C * M) / Math.pow(R + v, 2);  //pospesek
+		return a;
+	}
 }
